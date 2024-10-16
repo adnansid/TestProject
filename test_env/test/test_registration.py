a5e5387417bc81cdcfd39760b122575794b03973
adnansid <adnan.sid317@gmail.com>
2024-10-17 01:02:25 +0200

    App file

---
+++ test_env/test/test_registration.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+
+import pytest
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    yield driver
+
+    driver.quit()
+
+def test_registration(driver):
+    driver.get("http://localhost:3000/")
+    driver.maximize_window()
+    time.sleep(5)
+    driver.find_element(By.CSS_SELECTOR,"#work-2").click()
